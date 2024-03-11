935e4846433c88206f63af509ef1049e0bdaf55f
franklinthai <89907442+franklinthai@users.noreply.github.com>
2024-03-10 20:18:43 -0700

    more enviroment

--- Unity/Assets/Scripts/Interaction/random.cs
+++ Unity/Assets/Scripts/Interaction/random.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RandomInteractable : MonoBehaviour, IInteractable
 {
     // This method is called when the GameObject is interacted with
+    public string sceneName;
     public void Interact()
     {
-        // Generate and log a random number
-        int randomNumber = Random.Range(0, 100);
-        Debug.Log("Random number: " + randomNumber);
+        sceneName = "BattleScene";
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        // Load the scene
+        SceneManager.LoadScene(sceneName);
     }
 }
