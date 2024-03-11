5787bdfcbde568bfa6b1ff1a93a189f3a52bd97d
Arman <aafsoun@uw.edu>
2024-03-10 19:39:58 -0700

    Test

--- Unity/Assets/Scripts/BattleMode/BattleSystem.cs
+++ Unity/Assets/Scripts/BattleMode/BattleSystem.cs
@@ -7,134 +7,165 @@ public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
+    public GameObject playerObj;
+    public GameObject enemyObj;
 
-	public GameObject playerObj;
-	public GameObject enemyObj;
-
-	public Transform playerBattleStation;
-	public Transform enemyBattleStation;
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
 
     public Text dialogueText;
 
     public Character playerChar;
-	public Character enemyChar;
-	
-	public BattleHUD playerHUD;
-	public BattleHUD enemyHUD;
+    public Character enemyChar;
+    
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
 
     public BattleState state;
 
     public void Start()
     {
-		state = BattleState.START;
-		StartCoroutine(SetupBattle());
+        state = BattleState.START;
+        StartCoroutine(SetupBattle());
     }
 
     public IEnumerator SetupBattle()
-	{
-		GameObject playerGO = Instantiate(playerObj, playerBattleStation);
-		playerChar = playerGO.GetComponent<Character>();
+    {
+        GameObject playerGO = Instantiate(playerObj, playerBattleStation);
+        playerChar = playerGO.GetComponent<Character>();
 
-		GameObject enemyGO = Instantiate(enemyObj, enemyBattleStation);
-		enemyChar = enemyGO.GetComponent<Character>();
+        GameObject enemyGO = Instantiate(enemyObj, enemyBattleStation);
+        enemyChar = enemyGO.GetComponent<Character>();
 
-		dialogueText.text = "A wild " + enemyChar.charName + " approaches...";
+        dialogueText.text = "A wild " + enemyChar.charName + " approaches...";
 
-		playerHUD.SetHUD(playerChar);
-		enemyHUD.SetHUD(enemyChar);
+        playerHUD.SetHUD(playerChar);
+        enemyHUD.SetHUD(enemyChar);
 
-		yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
 
-		state = BattleState.PLAYERTURN;
-		PlayerTurn();
-	}
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
 
-	IEnumerator PlayerAttack()
-	{
-		bool isDead = enemyChar.TakeDamage(playerChar.damage);
+    void PlayerTurn()
+    {
+        dialogueText.text = "Choose an action: \n1. Attack Move 1\n2. Attack Move 2\n3. Attack Move 3\n4. Heal";
+    }
 
-		enemyHUD.SetHP(enemyChar.curHp);
-		dialogueText.text = "The attack is successful!";
+    public void OnAttackMove1Button()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
 
-		yield return new WaitForSeconds(2f);
+        StartCoroutine(PlayerAttackMove(1));
+    }
 
-		if(isDead)
-		{
-			state = BattleState.WON;
-			EndBattle();
-		} else
-		{
-			state = BattleState.ENEMYTURN;
-			StartCoroutine(EnemyTurn());
-		}
-	}
+    public void OnAttackMove2Button()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
 
-	IEnumerator EnemyTurn()
-	{
-		dialogueText.text = enemyChar.charName + " attacks!";
+        StartCoroutine(PlayerAttackMove(2));
+    }
 
-		yield return new WaitForSeconds(1f);
+    public void OnAttackMove3Button()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
 
-		bool isDead = playerChar.TakeDamage(enemyChar.damage);
+        StartCoroutine(PlayerAttackMove(3));
+    }
 
-		playerHUD.SetHP(playerChar.curHp);
+IEnumerator PlayerAttackMove(int moveNumber)
+{
+    bool isDead = false;
+    switch (moveNumber)
+    {
+        case 1:
+            isDead = playerChar.AttackMove1(enemyChar); // Player performs Attack Move 1 on enemy
+            dialogueText.text = "Attack Move 1 is successful!";
+            break;
+        case 2:
+            isDead = playerChar.AttackMove2(enemyChar); // Player performs Attack Move 2 on enemy
+            dialogueText.text = "Attack Move 2 hits hard!";
+            break;
+        case 3:
+            isDead = playerChar.AttackMove3(enemyChar); // Player performs Attack Move 3 on enemy
+            dialogueText.text = "Attack Move 3 strikes!";
+            break;
+    }
 
-		yield return new WaitForSeconds(1f);
+    enemyHUD.SetHP(enemyChar.curHp); // Update enemy HP HUD
 
-		if(isDead)
-		{
-			state = BattleState.LOST;
-			EndBattle();
-		} else
-		{
-			state = BattleState.PLAYERTURN;
-			PlayerTurn();
-		}
+    yield return new WaitForSeconds(2f);
 
-	}
+    if (isDead)
+    {
+        state = BattleState.WON;
+        EndBattle();
+    }
+    else
+    {
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+}
 
-	void EndBattle()
-	{
-		if(state == BattleState.WON)
-		{
-			dialogueText.text = "You won the battle!";
-		} else if (state == BattleState.LOST)
-		{
-			dialogueText.text = "You were defeated.";
-		}
-	}
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
 
-	void PlayerTurn()
-	{
-		dialogueText.text = "Choose an action:";
-	}
+        StartCoroutine(PlayerHeal());
+    }
 
-	IEnumerator PlayerHeal()
-	{
-		playerChar.Heal(20);
+    IEnumerator PlayerHeal()
+    {
+        playerChar.Heal(20);
 
-		playerHUD.SetHP(playerChar.curHp);
-		dialogueText.text = "You feel renewed strength!";
+        playerHUD.SetHP(playerChar.curHp);
+        dialogueText.text = "You feel renewed strength!";
 
-		yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(2f);
 
-		state = BattleState.ENEMYTURN;
-		StartCoroutine(EnemyTurn());
-	}
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
 
-	public void OnAttackButton()
-	{
-		if (state != BattleState.PLAYERTURN)
-			return;
+    IEnumerator EnemyTurn()
+    {
+        dialogueText.text = enemyChar.charName + " attacks!";
 
-		StartCoroutine(PlayerAttack());
-	}
+        yield return new WaitForSeconds(1f);
 
-	public void OnHealButton()
-	{
-		if (state != BattleState.PLAYERTURN)
-			return;
+        bool isDead = playerChar.TakeDamage(enemyChar.damage);
 
-		StartCoroutine(PlayerHeal());
-	}
+        playerHUD.SetHP(playerChar.curHp);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogueText.text = "You won the battle!";
+        }
+        else if (state == BattleState.LOST)
+        {
+            dialogueText.text = "You were defeated.";
+        }
+    }
 }
--- Unity/Assets/Scripts/BattleMode/Character.cs
+++ Unity/Assets/Scripts/BattleMode/Character.cs
@@ -7,23 +7,37 @@ public class Character : MonoBehaviour
     public string charName;
     public int maxHp;
     public int curHp;
+    public int damage; // Consider removing if not used elsewhere
+    public int attackMove1Damage;
+    public int attackMove2Damage;
+    public int attackMove3Damage; // Added third attack move
     
-    public int damage;
-    
-    // Returns true if char dead, false otherwise
     public bool TakeDamage(int dmg)
-	{
+    {
         curHp -= dmg;
-        if (curHp <= 0)
-			return true;
-		else
-			return false;
+        return curHp <= 0;
     }
 
     public void Heal(int amount)
-	{
-		curHp += amount;
-		if (curHp > maxHp)
-			curHp = maxHp;
-	}
+    {
+        curHp += amount;
+        if (curHp > maxHp)
+            curHp = maxHp;
+    }
+
+    public bool AttackMove1(Character target)
+    {
+        return target.TakeDamage(attackMove1Damage);
+    }
+
+
+    public bool AttackMove2(Character target)
+    {
+        return target.TakeDamage(attackMove2Damage);
+    }
+
+    public bool AttackMove3(Character target) // New method for third attack move
+    {
+        return target.TakeDamage(attackMove3Damage);
+    }
 }
